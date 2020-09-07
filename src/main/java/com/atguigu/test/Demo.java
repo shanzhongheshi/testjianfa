83889176f42a844b3a92843b0361d4de76756da0
shanzhongheshi <869060393@qq.com>
2020-09-07 19:18:07 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/test/Demo.java
@@ -0,0 +1,11 @@
+package com.atguigu.test;
+
+/**
+ * @author Lin
+ * 2020/9/7  19:13
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("刘亦菲是最漂亮的花木兰");
+    }
+}
